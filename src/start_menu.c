8c986de069959a5b85240ef6744edfd93515a0e1
LuckyCapybara <215329213+LuckyCapybara@users.noreply.github.com>
2025-06-24 17:15:08 -0600

    REMOVE THIS: Add ShuffleTime minigame to menu for debug

--- src/start_menu.c
+++ src/start_menu.c
@@ -49,6 +49,7 @@
 #include "constants/battle_frontier.h"
 #include "constants/rgb.h"
 #include "constants/songs.h"
+#include "shuffle_time.h"
 
 // Menu actions
 enum
@@ -68,6 +69,7 @@ enum
     MENU_ACTION_PYRAMID_BAG,
     MENU_ACTION_DEBUG,
     MENU_ACTION_DEXNAV,
+    MENU_ACTION_SHUFFLE_TIME,
 };
 
 // Save status
@@ -110,6 +112,7 @@ static bool8 StartMenuBattlePyramidRetireCallback(void);
 static bool8 StartMenuBattlePyramidBagCallback(void);
 static bool8 StartMenuDebugCallback(void);
 static bool8 StartMenuDexNavCallback(void);
+static bool8 StartMenuShuffleTimeCallback(void);
 
 // Menu callbacks
 static bool8 SaveStartCallback(void);
@@ -205,6 +208,7 @@ static const struct MenuAction sStartMenuItems[] =
     [MENU_ACTION_PYRAMID_BAG]     = {gText_MenuBag,     {.u8_void = StartMenuBattlePyramidBagCallback}},
     [MENU_ACTION_DEBUG]           = {sText_MenuDebug,   {.u8_void = StartMenuDebugCallback}},
     [MENU_ACTION_DEXNAV]          = {gText_MenuDexNav,  {.u8_void = StartMenuDexNavCallback}},
+    [MENU_ACTION_SHUFFLE_TIME]    = {sText_MenuDebug,   {.u8_void = StartMenuShuffleTimeCallback}},
 };
 
 static const struct BgTemplate sBgTemplates_LinkBattleSave[] =
@@ -344,6 +348,7 @@ static void BuildNormalStartMenu(void)
     if (FlagGet(FLAG_SYS_POKENAV_GET) == TRUE)
         AddStartMenuAction(MENU_ACTION_POKENAV);
 
+    AddStartMenuAction(MENU_ACTION_SHUFFLE_TIME);
     AddStartMenuAction(MENU_ACTION_PLAYER);
     AddStartMenuAction(MENU_ACTION_SAVE);
     AddStartMenuAction(MENU_ACTION_OPTION);
@@ -1494,7 +1499,13 @@ void AppendToList(u8 *list, u8 *pos, u8 newEntry)
 
 static bool8 StartMenuDexNavCallback(void)
 {
-    CreateTask(Task_OpenDexNavFromStartMenu, 0);
+    CreateTask(Task_OpenShuffleTime, 0);
+    return TRUE;
+}
+
+static bool8 StartMenuShuffleTimeCallback(void)
+{
+    CreateTask(Task_OpenShuffleTime, 0);
     return TRUE;
 }
 
